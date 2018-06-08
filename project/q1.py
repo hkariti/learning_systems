69dd6072133dd33d46507807e46127ca085590af
Hagai Kariti <hkariti@gmail.com>
2018-06-08 22:52:04 +0300

    Update samples, write q1 runnable code

---
+++ project/q1.py
@@ -0,0 +1,45 @@
+import numpy
+import matplotlib.pyplot as plt
+
+import samples
+import kmeans
+
+K = 2
+
+def graph_kmeans(classes):
+    k = len(classes)
+    legend = []
+    plt.figure()
+    for i in range(k):
+        selected = samples.X_2d[classes[i]]
+        x = selected[...,0]
+        y = selected[...,1]
+        legend.append("Class {}".format(i+1))
+        plt.scatter(x, y, s=3)
+    plt.legend(legend)
+    plt.title("K-mean with K={} in 2d".format(k))
+    plt.xlabel("Component 1")
+    return plt.ylabel("Component 2")
+
+def run_kmeans_on_all():
+    print("Running kmeans on all samples with K={0}".format(K))
+    classes,_,_,_ = kmeans.kmeans(samples.X, K)
+    print("Class 1: ", numpy.nonzero(classes[0])[0])
+    print("Class 2: ", numpy.nonzero(classes[1])[0])
+
+def run_kmeans_2d():
+    print("Running kmeans in all samples in 2d with K={0}".format(K))
+    classes,_,_,_ = kmeans.kmeans(samples.X_2d, K)
+
+    plt_tagged = samples.graph_tagged_samples_2d()
+    plt_kmeans = graph_kmeans(classes)
+
+    new_k = 5
+    print("Running kmeans in all samples in 2d with K={0}".format(new_k))
+    classes,_,_,_ = kmeans.kmeans(samples.X_2d, new_k)
+    plt_kmeans_new_k = graph_kmeans(classes)
+
+    return plt_tagged, plt_kmeans, plt_kmeans_new_k
+
+run_kmeans_on_all()
+run_kmeans_2d()
--- project/samples.py
+++ project/samples.py
@@ -1,12 +1,31 @@
 import numpy
 import scipy.io
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 bmat = scipy.io.loadmat("BreastCancerData.mat")
 
-X = bmat['X']
+X = numpy.transpose(bmat['X'])
 y = bmat['y']
+
+_pca = PCA(n_components=2)
+X_2d = _pca.fit_transform(X)
+
 _test_indices = [3,11,17,21,22,28,29,41,44,46,68,69,70,77,84,95,116,124,127,130,133,139,144,145,147,162,166,168,173,183,184,185,188,195,198,208,212,213,215,221,222,226,227,230,234,243,244,247,252,256,257,259,261,263,266,271,275,276,285,292,293,297,298,307,309,310,313,318,321,326,337,345,346,356,358,362,363,364,374,377,378,379,381,389,414,416,423,438,443,446,456,462,465,467,469,477,478,480,486,489,492,493,505,514,516,518,519,520,523,524,525,538,549,553,568]
 test_set_idx = numpy.zeros(len(y), dtype=numpy.bool)
 test_set_idx[_test_indices] = 1
 
 training_set_idx = numpy.ones(len(y), dtype=numpy.bool) ^ test_set_idx
+
+def graph_tagged_samples_2d():
+    scatter_x = X_2d[...,0]
+    scatter_y = X_2d[...,1]
+    good_indices = (y == 0)[...,0]
+    bad_indices = numpy.invert(good_indices)
+    plt.figure()
+    plt.scatter(scatter_x[good_indices],scatter_y[good_indices], c='b', s=3)
+    plt.scatter(scatter_x[bad_indices], scatter_y[bad_indices], c='r', s=3)
+    plt.xlabel("Component 1")
+    plt.ylabel("Component 2")
+    plt.title("Tagged samples in 2d")
+    return plt.legend(["Benign", "Malignant"])
