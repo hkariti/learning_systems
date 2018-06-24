b4e592d456ec815aeb57e89b774a1c08bfcaf817
Hagai Kariti <hkariti@gmail.com>
2018-06-24 23:59:40 +0300

    Use square distance error

--- project/partB/NeuralNet.py
+++ project/partB/NeuralNet.py
@@ -14,15 +14,18 @@ class NeuralNet:
         return 1*(x > 0)
 
     def tanh_d(x):
-        return 0.5*(1-tanh(x))*(1+tanh(x))
+        return 0.5*(1-NeuralNet.tanh(x))*(1+NeuralNet.tanh(x))
 
     def log_d(x):
-        return log(x)*(1-log(x))
+        return NeuralNet.log(x)*(1-NeuralNet.log(x))
 
-    def logstic_regression(x):
-        s1 = 1/(1+np.exp(x[0]-x[1]))
-        s2 = 1/(1+np.exp(x[1]-x[0]))
-        return np.array([s1, s2])
+    def softmax(x):
+        log_result = -np.logaddexp(0, -x)
+        result = np.exp(log_result)
+        return result
+    
+    def softmax_d(x):
+        return NeuralNet.softmax(x)*(1-NeuralNet.softmax(x))
 
     FUNCS = dict(ReLU=ReLU, log=log, tanh=tanh)
     DERIVATIVES = dict(ReLU=ReLU_d, log=log_d, tanh=tanh_d)
@@ -41,9 +44,9 @@ class NeuralNet:
 
         self.input_layer_size = input_layer_size
         self.middle_layer_size = middle_layer_size
-        self.output_layer_size = 2
-        self.activation_funcs = [self.FUNCS[activation_func], NeuralNet.logstic_regression]
-        self.derivative_func = self.DERIVATIVES[activation_func]
+        self.output_layer_size = 1
+        self._activation_funcs = [self.FUNCS[activation_func], NeuralNet.softmax]
+        self._derivative_funcs = [self.DERIVATIVES[activation_func], NeuralNet.softmax_d]
 
         # Init wights and biases.
         # In each weight matrix, each row is a neuron in the receiving layer.
@@ -51,21 +54,20 @@ class NeuralNet:
         self.weights = []
         self.biases = []
         # Init weights for input->hidden
-        self.weights.append(np.zeros((middle_layer_size, input_layer_size)))
+        self.weights.append(np.random.random((middle_layer_size, input_layer_size)))
         self.biases.append(np.zeros(middle_layer_size))
         # Init weights for hidden->output
-        self.weights.append(np.zeros((self.output_layer_size, middle_layer_size)))
+        self.weights.append(np.random.random((self.output_layer_size, middle_layer_size)))
         self.biases.append(np.zeros(self.output_layer_size))
-
-        # Init data structures for BP
-        self._outputs = []
-        self._inputs = []
-        self._deltas = []
 
     def _feed_forward(self, samples):
         layer_count = len(self.weights)
         if samples.ndim == 1:
             samples = np.array([samples])
+        # Init data structures for BP
+        self._outputs = []
+        self._inputs = []
+        # Feed data into the net
         outs = []
         for sample in samples:
             outputs = []
@@ -74,7 +76,7 @@ class NeuralNet:
             for layer in range(layer_count):
                 weights = self.weights[layer]
                 biases = self.biases[layer]
-                activation_func = self.activation_funcs[layer]
+                activation_func = self._activation_funcs[layer]
                 prev_out = out
                 outputs.append(prev_out)
                 v = weights.dot(prev_out) + biases
@@ -90,32 +92,31 @@ class NeuralNet:
         """
         Classify samples. samples should be a vector of size input_layer_size or a matrix of samples, with each sample being a row with input_layer_size columns.
         """
-        return np.argmax(self._feed_forward(samples))
+        return np.round(self._feed_forward(samples))
 
-    def _cross_entropy(self, output, output_tags):
-        return -np.sum(output_tags*np.log(output[...,0]) + ((1-output_tags)*np.log(output[...,1])))
+    def _error(self, output, output_tags):
+        return np.sum((output_tags-output[...,0])**2)
 
     def _calculate_gradient_batch(self, output, output_tags):
         # Calculate the gradient for each layer's weight matrix. This could probably be done in a more generic way and much more efficiently using
         # matrices properly, but matrices confuse me so for loops it is.
-        d_weights_0 = 0
         d_weights_1 = np.zeros((self.output_layer_size, self.middle_layer_size))
         d_weights_0 = np.zeros((self.middle_layer_size, self.input_layer_size))
-        for sample_idx in range(len(output)):
-            # Calculate delta for output layer
-            deltas_out = np.zeros(self.output_layer_size)
-            # Calculate gradient for each input (outer for is redundant atm because we're hardcoded to 1 output neuron)
-            for output_neuron in range(self.output_layer_size):
-                deltas_out[output_neuron] = output_tags[sample_idx] - output[sample_idx][output_neuron]
-                for hidden_neuron in range(self.middle_layer_size):
-                    addition_from_current_sample = deltas_out[output_neuron] * self._outputs[sample_idx][1][hidden_neuron]
-                    d_weights_1[output_neuron, hidden_neuron] = d_weights_1[output_neuron, hidden_neuron] - addition_from_current_sample
+        sample_idx = np.random.randint(len(output))
+        # Calculate delta for output layer
+        deltas_out = np.zeros(self.output_layer_size)
+        # Calculate gradient for each input (outer for is redundant atm because we're hardcoded to 1 output neuron)
+        for output_neuron in range(self.output_layer_size):
+            deltas_out[output_neuron] = (output_tags[sample_idx] - output[sample_idx][output_neuron])*self._derivative_funcs[1](self._inputs[sample_idx][1][output_neuron])
+            for hidden_neuron in range(self.middle_layer_size):
+                addition_from_current_sample = deltas_out[output_neuron] * self._outputs[sample_idx][1][hidden_neuron]
+                d_weights_1[output_neuron, hidden_neuron] = -addition_from_current_sample
 
-            # Calculate delta for each neuron in the hidden layer and gradient for each of their inputs
-            for neuron in range(self.middle_layer_size):
-                delta = self.derivative_func(self._inputs[sample_idx][0][neuron])*np.sum(self.weights[1][:,neuron]*deltas_out)
-                for input_neuron in range(self.input_layer_size):
-                    d_weights_0[neuron, input_neuron] = d_weights_0[neuron, input_neuron] - (delta * self._outputs[sample_idx][0][input_neuron])
+        # Calculate delta for each neuron in the hidden layer and gradient for each of their inputs
+        for neuron in range(self.middle_layer_size):
+            delta = self._derivative_funcs[0](self._inputs[sample_idx][0][neuron])*np.sum(self.weights[1][:,neuron]*deltas_out)
+            for input_neuron in range(self.input_layer_size):
+                d_weights_0[neuron, input_neuron] = - (delta * self._outputs[sample_idx][0][input_neuron])
 
         return [d_weights_0, d_weights_1]
 
@@ -127,11 +128,11 @@ class NeuralNet:
         Will raise RuntimeError if learning didn't complete after max_iterations iterations.
         """
         output = self._feed_forward(training_set)
-        current_error = self._cross_entropy(output, training_tags)
+        current_error = self._error(output, training_tags)
         prev_error = 0
         learning_progress = [current_error]
         iterations = 0
-        while (current_error > threshold and (current_error - prev_error) > threshold):
+        while (current_error > threshold and np.abs(current_error - prev_error) > threshold):
             iterations = iterations + 1
             if (iterations > max_iterations):
                 raise RuntimeError("Didn't converge to threshold {} after {} iterations. Current error is {}".format(threshold, max_iterations, current_error))
@@ -140,16 +141,18 @@ class NeuralNet:
             self.weights[1] = self.weights[1] - learning_rate * gradient_1
             output = self._feed_forward(training_set)
             prev_error = current_error
-            current_error = self._cross_entropy(output, training_tags)
+            current_error = self._error(output, training_tags)
             learning_progress.append(current_error)
 
+        print("Cur:", current_error > threshold)
+        print("CurPrev:", current_error - prev_error > threshold)
         return learning_progress
 
     def test_model(self, test_set, test_tags):
         """
         Test the model with the given test_set against the given tags and return the error.
 
-        Error is calculated using cross-entropy.
+        Error is calculated using square error.
         """
         output = self._feed_forward(test_set)
-        return self._cross_entropy(output, test_tags)
+        return self._error(output, test_tags)
